129e308d8473665f7c3d6c76417aeb3126031580
Shagun Dubey <1202shagun@gmail.com>
2023-09-08 09:21:41 +0530

    navigation design is added

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,13 +1,27 @@
-
-
 const Navigation = () => {
-    return <header>
-        <nav>
-            <ul>
-                <li className=" text-primary-color text-h1 ">first list</li>
-            </ul>
-        </nav>
-    </header>;
+    return (
+        <header className=" bg-black relative w-full flex justify-center items-center flex-row">
+            <nav className=" relative w-full flex justify-between items-center p-4 max-w-[1140px]">
+                <div>
+                    <h4 className=" text-primary-color text-3xl uppercase">
+                        Hotel Galaxy
+                    </h4>
+                </div>
+                <div className=" flex justify-center items-center gap-8">
+                    <ul className=" text-white flex justify-center items-center gap-5 text-xl font-semibold font-['Josefin_Sans']">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Rooms</li>
+                        <li>Meetings & Events</li>
+                        <li>Contact</li>
+                    </ul>
+                    <button className=" bg-primary-color px-6 py-3 text-white text-md uppercase hover:bg-white hover:text-primary-color transition-all duration-200 ease-in-out tracking-wide">
+                        Book Now
+                    </button>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navigation;
